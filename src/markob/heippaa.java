5f1c46ba47773201c7c9dc1d23eccc447451aac5
Marko Back <marko.back@tieto.com>
2015-01-14 11:54:12 +0200

    testicommit

---
+++ src/markob/heippaa.java
@@ -0,0 +1,13 @@
+package markob;
+
+public class heippaa {
+
+    public static void main(String[] args) {
+        
+        for (int i=1;i <= 100; i++){
+            System.out.println("HELOUUU MAAILMA" + i);
+        }
+
+    }
+
+}
